39f928da63f7f14f04ab6c78811fdbf2b8b7d3d7
WilfriedB <WilfriedB@users.noreply.github.com>
2016-08-15 23:24:59 +0200

    WPF Gui toegevoegd

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Library;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var bm = new WriteableBitmap(320, 320, 120, 120, PixelFormats.Bgr32, null);
+            SpiraalImage.Source = bm;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var generator = new CoordinateGenerator(60);
+            var bm = (WriteableBitmap)SpiraalImage.Source;
+            byte[] colorData = { 255, 255, 255, 255 };
+            int stride = bm.PixelWidth * bm.Format.BitsPerPixel / 8;
+            foreach (var item in generator.GetCoordinates())
+            {
+                var rect = new Int32Rect(item.Item2+160, item.Item3+160, 1, 1);
+                bm.WritePixels(rect, colorData, stride, 0);
+            }
+        }
+    }
+}
