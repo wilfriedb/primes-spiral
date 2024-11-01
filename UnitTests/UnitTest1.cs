6ec736714526094f9bc32e89f4e9b4ae8d7b2c7f
Wilfried Boos <wjgboos@outlook.com>
2024-11-01 22:04:31 +0100

    Migrate to dotnet 8

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,63 +1,61 @@
 ﻿using System;
 using Library;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
-namespace UnitTests
+namespace UnitTests;
+
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+    [Fact]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
+        var expected = new[]
         {
-            var expected = new[]
-            {
-                // Arrange
-                Tuple.Create(0,0,0),
-                Tuple.Create(1,1,0),
-                Tuple.Create(2,1,-1),
-                Tuple.Create(3,0,-1),
-                Tuple.Create(4,-1,-1),
-                Tuple.Create(5,-1,0),
-                Tuple.Create(6,-1,1),
-                Tuple.Create(7,0,1),
-                Tuple.Create(8,1,1),
-                Tuple.Create(9,2,1),
-                Tuple.Create(10,2,0),
-                Tuple.Create(11,2,-1),
-                Tuple.Create(12,2,-2),
-                Tuple.Create(13,1,-2),
-                Tuple.Create(14,0,-2),
-                Tuple.Create(15,-1,-2),
-                Tuple.Create(16,-2,-2),
-                Tuple.Create(17,-2,-1),
-                Tuple.Create(18,-2,0),
-                Tuple.Create(19,-2,1),
-                Tuple.Create(20,-2,2),
-                Tuple.Create(21,-1,2),
-                Tuple.Create(22,0,2),
-                Tuple.Create(23,1,2),
-                Tuple.Create(24,2,2),
-                Tuple.Create(25,3,2)
-            };
-            var generator = new CoordinatesGenerator(3);
+            // Arrange
+            Tuple.Create(0,0,0),
+            Tuple.Create(1,1,0),
+            Tuple.Create(2,1,-1),
+            Tuple.Create(3,0,-1),
+            Tuple.Create(4,-1,-1),
+            Tuple.Create(5,-1,0),
+            Tuple.Create(6,-1,1),
+            Tuple.Create(7,0,1),
+            Tuple.Create(8,1,1),
+            Tuple.Create(9,2,1),
+            Tuple.Create(10,2,0),
+            Tuple.Create(11,2,-1),
+            Tuple.Create(12,2,-2),
+            Tuple.Create(13,1,-2),
+            Tuple.Create(14,0,-2),
+            Tuple.Create(15,-1,-2),
+            Tuple.Create(16,-2,-2),
+            Tuple.Create(17,-2,-1),
+            Tuple.Create(18,-2,0),
+            Tuple.Create(19,-2,1),
+            Tuple.Create(20,-2,2),
+            Tuple.Create(21,-1,2),
+            Tuple.Create(22,0,2),
+            Tuple.Create(23,1,2),
+            Tuple.Create(24,2,2),
+            Tuple.Create(25,3,2)
+        };
+        var generator = new CoordinatesGenerator(3);
 
-            // Act
-            var enumerator = generator.GetCoordinates().GetEnumerator();
+        // Act
+        var enumerator = generator.GetCoordinates().GetEnumerator();
 
-            // Assert
-            foreach (var a in expected)
+        // Assert
+        foreach (var a in expected)
+        {
+            if (enumerator.MoveNext())
             {
-                if (enumerator.MoveNext())
-                {
-                    Assert.AreEqual(a, enumerator.Current);
-                }
-                else
-                {
-                    break;
-                }
+                Assert.Equal(a, enumerator.Current);
             }
-
+            else
+            {
+                break;
+            }
         }
+
     }
 }
